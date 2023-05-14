bef7b5a950fbb046453c20bbd9a90fd05c0ae651
Your Name <saifi.mujeeb2@gmail.com>
2023-05-14 14:19:32 +0530

    Added: any mood message gives happy mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,13 +1,15 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+
     public String analyzeMood(String message) {
         String mood = null;
         String[] messageArr = message.split(" ");
         for (int i = 0; i < messageArr.length; i++) {
-            if (messageArr[i].equals("Happy"))
+            if ((messageArr[i].equals("Happy"))||(messageArr[i].equals("Any"))) {
                 mood = "Happy";
-            else
+                break;
+            }else
                 mood = "Sad";
         }
         return mood;
